f9b19a921d748b27d55a28c56c52e6091ea28580
ValdemarAndelius <118256888+ValdemarAndelius@users.noreply.github.com>
2023-03-06 14:50:10 +0100

    h

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
-const burger = document.getElementById('burger')
-const nav = document.getElementById('nav-links')
 
-function toggleMenu() {
-    burger.classList.toggle('burger_toggle')
-    nav.classList.toggle('nav_active')
+
+
+
+const burger = document.querySelector('#burger');
+const navLinks = document.querySelector('#nav-links');
+
+function toggleNav() {
+  burger.classList.toggle('active');
+  navLinks.classList.toggle('nav_active');
 }
 
-burger.addEventListener('click', toggleMenu)
-
-
-var btn = $('.btn');
-
-btn.on('click', function() {
-  $(this).toggleClass('active not-active');
-});
-
+burger.addEventListener('click', toggleNav);
